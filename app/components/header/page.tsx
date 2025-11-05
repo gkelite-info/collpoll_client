d346cbd8477332de5858b21685bbe7ea908fc329
gkelite-info <gkeliteinfo@gmail.com>
2025-11-05 12:30:52 +0530

    added layout

---
+++ app/components/header/page.tsx
@@ -0,0 +1,13 @@
+
+
+
+
+export default function Header() {
+    return (
+        <>
+            <div className="h-[100%] bg-pink-400">
+                Its header
+            </div>
+        </>
+    )
+}
