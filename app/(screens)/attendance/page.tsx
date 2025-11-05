af3807884acfefd3ca068e28e4d20314419af6f1
gkelite-info <gkeliteinfo@gmail.com>
2025-11-05 14:42:21 +0530

    attendance

---
+++ app/(screens)/attendance/page.tsx
@@ -0,0 +1,17 @@
+export default function Attendance() {
+  return (
+    <div className="bg-[#EFEFEF] flex justify-center w-full h-full p-3">
+      <div className="flex flex-col w-[70%]">
+        <div>
+          <h1 className="text-[#282828] font-bold text-2xl">Attendance</h1>
+          <p className="text-[#282828]">
+            Track, Manage, and Maintain Your Attendance Effortlessly
+          </p>
+        </div>
+        <div></div>
+        <div></div>
+      </div>
+      <div className="w-[30%]"></div>
+    </div>
+  )
+}
