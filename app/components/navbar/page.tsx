a3e3324996dac3b5be14c7a275b86fb0e8cde26e
gkelite-info <gkeliteinfo@gmail.com>
2025-11-05 13:01:04 +0530

    changed layout

--- app/components/navbar/page.tsx
+++ app/components/navbar/page.tsx
@@ -4,7 +4,7 @@
 export default function Navbar() {
     return (
         <>
-            <div className="bg-[#43C17A]">
+            <div className="bg-[#43C17A] flex justify-start">
                 Its navbar
             </div>
         </>
