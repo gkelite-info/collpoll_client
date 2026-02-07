0165d6d1ef43a60cb9d55b192c044c7a140c02f4
saisaraswathi-gif <saisaraswathi@gkeliteinfo.com>
2026-02-07 15:11:25 +0530

    Student Attendance 2nd screen & 3rd screen

--- app/(screens)/(attendance)/subject-attendance-details/page.tsx
+++ app/(screens)/(attendance)/subject-attendance-details/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import CardComponent from "@/app/utils/card";
 import { useUser } from "@/app/utils/context/UserContext";
 import CourseScheduleCard from "@/app/utils/CourseScheduleCard";
