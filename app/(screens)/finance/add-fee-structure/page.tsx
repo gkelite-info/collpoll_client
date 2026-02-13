af8335faf0225aa83bc42c6912a9791159e41cc9
gkelite-info <gkeliteinfo@gmail.com>
2026-02-13 10:55:10 +0530

    added fee structure in navbar

---
+++ app/(screens)/finance/add-fee-structure/page.tsx
@@ -0,0 +1,9 @@
+export default function AddFeeStructure() {
+    return (
+        <>
+            <div className="bg-red-400 h-screen">
+                <p>Add Fee structure</p>
+            </div>
+        </>
+    )
+}
