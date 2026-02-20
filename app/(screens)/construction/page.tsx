25f9df9d4508bdee7704e2e7f208e47dfcb9fa0f
gkelite-info <gkeliteinfo@gmail.com>
2026-02-20 16:13:24 +0530

    added construction

---
+++ app/(screens)/construction/page.tsx
@@ -0,0 +1,9 @@
+export default function UnderConstruction() {
+    return (
+        <>
+            <div className="flex items-center justify-center h-screen">
+                <img src="/construction.jpg" alt="construction" />
+            </div>
+        </>
+    )
+}
